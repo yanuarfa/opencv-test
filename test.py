d504507d6f86cee1e0ff71a11ba829232f786a6b
Yanuar Faturahman <51121711+yanuarfa@users.noreply.github.com>
2022-09-30 17:31:03 +0700

    link image

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2
 
-path=r'D:\kucing.jpg';
+path=r'D:\kucing.jpg'; #https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/10/28064854/12.-Tips-Merawat-Anak-Kucing-Munchkin.jpg
 cv_read=cv2.imread(path);
 cv2.imshow("ini kucing",cv_read);
 
