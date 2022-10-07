822f3ad62c19230c7a853d1a0b87bee441d0a3ed
yanuarfa <yanuarfatur41@gmail.com>
2022-10-07 17:50:01 +0700

    added bright grayscale

--- LearnOpenCV/pcd-02.py
+++ LearnOpenCV/pcd-02.py
@@ -11,13 +11,17 @@ Blue = cv_read[:,:,1]
 
 height = cv_read.shape[0]
 width = cv_read.shape[1]
+constant_value = np.int32(50)
 
-grayscale = np.zeros([height, width], dtype=np.int32())
+grayscale = np.zeros([height, width], dtype = np.int32())
+gray_bright = np.zeros([height, width], dtype = np.int32())
 
 for row in range (1, height):
     for col in range (1, width):
         grayscale[row, col] = (0.299 * Red[row, col]) + (0.587 * Green[row, col]) + (0.114 * Blue[row, col])
+        gray_bright[row, col] = grayscale[row, col] + constant_value
 
 cv2.imshow('Grayscale', np.uint8(grayscale))
+cv2.imshow('Grayscale Bright', np.uint8(gray_bright))
 cv2.waitKey(0)
 cv2.destroyAllWindows
